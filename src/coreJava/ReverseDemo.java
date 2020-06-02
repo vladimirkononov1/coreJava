a4d94c41bfaf7bb09f4b9c624ceb5b623d76b8fb
Vlad Kononov <vladimirkon@gmail.com>
2020-06-01 17:26:18 -0700

    added ReverseDemo case

---
+++ src/coreJava/ReverseDemo.java
@@ -0,0 +1,22 @@
+package coreJava;
+
+public class ReverseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "civic";
+		String reverse = "";
+		for(int count= name.length()-1; count >= 0; count--) {
+			reverse = reverse + name.charAt(count);
+		}
+		System.out.println(reverse);
+		
+		if(name.equalsIgnoreCase(reverse)) {
+			System.out.println(name + " is a palindrome word");
+		} else {
+			System.out.println(name + " is NOT a palindrome word!!!");
+		}
+		
+	}
+
+}
